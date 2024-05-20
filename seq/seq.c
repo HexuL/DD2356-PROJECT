2721eec430bb17640f11ede5ab27c31dfe0284c9
HohsuLi <li.hexu@outlook.com>
2024-05-20 20:19:38 +0200

    test2

--- seq/seq.c
+++ seq/seq.c
@@ -55,7 +55,7 @@ int main() {
         t += dt;
 
         printf("Time: %f\n", t);
-            // Create directory if it doesn't existed1
+            // Create directory if it doesn't existed
         const char *dirName = "outputdata";
         #ifdef _WIN32
             mkdir(dirName);
